69eef59acc092fdcd97049102d2334320a15f3b9
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-06-16 21:34:53 +0200

    views nearly connect to backend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const fetch = require("node-fetch");
 require('dotenv').config();
 
 const app = express();
@@ -14,7 +15,45 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-  res.render("index", { user: req.user });
+  res.render("index", { data: null });
+});
+
+app.post('/search', async (req, res) => {
+  const baseUrl = "https://api.openweathermap.org";
+  const limit = 1;
+  const units = "imperial";
+  const city = req.body.searchTerm; // Assuming the input field name is 'searchTerm'
+  const apiKey = process.env.OPENWEATHER_API_KEY;
+  
+  try {
+    const response = await fetch(`${baseUrl}/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`);
+    const data = await response.json();
+    const { lat, lon } = data[0];
+    const { name, state, country } = data[0];
+
+    const response2 = await fetch(`${baseUrl}/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`);
+    const data2 = await response2.json();
+    const { main, wind } = data2;
+
+    const response3 = await fetch(`${baseUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`);
+    const data3 = await response3.json();
+    const hourly = data3.list;
+
+    const weatherData = {
+      name,
+      state,
+      country,
+      main,
+      wind,
+      units,
+      hourly
+    };
+    
+    res.render("index", { data: weatherData });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
